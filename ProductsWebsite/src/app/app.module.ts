393b88697444d00e28cd56788e68d51382fcd345
matan27500n <matan27500n@gmail.com>
2020-08-16 13:52:45 +0300

    project with asaf

---
+++ ProductsWebsite/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule,AppRoutingModule],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
