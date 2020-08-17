efaeced40d720b12f2f1d333661b7b74d32a7630
matan27500n <matan27500n@gmail.com>
2020-08-17 10:35:36 +0300

    site 15 70 15

--- ProductsWebsite/src/app/app.module.ts
+++ ProductsWebsite/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [],
-  imports: [BrowserModule,AppRoutingModule],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, MenuComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: []
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
