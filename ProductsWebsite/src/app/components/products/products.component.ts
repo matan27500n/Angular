e059bc46025f6e4bdf8b4e9147f9e8bd777e2e6a
matan27500n <matan27500n@gmail.com>
2020-08-21 16:20:34 +0300

    observable

--- ProductsWebsite/src/app/components/products/products.component.ts
+++ ProductsWebsite/src/app/components/products/products.component.ts
@@ -1,41 +1,51 @@
 import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ProductsService} from 'src/app/services/products.service';
+import { ProductsService } from 'src/app/services/products.service';
 import { from } from 'rxjs';
 import { error } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+  public products: Product[];
 
-  public products : Product[];
-
- public constructor(private title: Title,private productsService: ProductsService) { }
+  public constructor(
+    private title: Title,
+    private productsService: ProductsService
+  ) {}
 
   public ngOnInit(): void {
-    this.title.setTitle("Products site");
+    this.title.setTitle('Products site');
 
     //קריאה סינכרונית
-   // this.products = this.productsService.getAllProducts();
+    // this.products = this.productsService.getAllProducts();
 
+    //קריאה אסינכרונית - טכניקה ראשונה
+    // this.productsService.getAllProductsAsync(products => {
+    //  this.products = products;
+    //  }, error => {
+    //   alert("Error: " + error.message);
+    //  });
 
-   //קריאה אסינכרונית - טכניקה ראשונה
-  // this.productsService.getAllProductsAsync(products => {
-   //  this.products = products;
- //  }, error => {
-  //   alert("Error: " + error.message);
- //  });
+    //קריאה אסינכרונית - טכניקה שנייה
+    // this.productsService.getAllProductsAsync2().then(products => {
+    //  this.products = products;
+    // }).catch(error => {
+    //  alert("Error: " + error.message);
+    // });
 
- //קריאה אסינכרונית - טכניקה שנייה
- this.productsService.getAllProductsAsync2().then(products => {
-   this.products = products;
- }).catch(error => {
-  alert("Error: " + error.message);
- });
-
-}
+    //קריאה אסינכרונית - טכניקה שלישית
+    this.productsService.getAllProductsAsync3().subscribe(
+      (products) => {
+        this.products = products;
+      },
+      (error) => {
+        alert('Error: ' + error.message);
+      }
+    );
+  }
 }
--- ProductsWebsite/src/app/services/products.service.ts
+++ ProductsWebsite/src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
+import { Observable, observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -31,6 +32,7 @@ export class ProductsService {
     }, 3000);
   }
 
+  //promise
   public getAllProductsAsync2(): Promise<Product[]>{
 
     return new Promise((resolve,reject) =>{
@@ -46,6 +48,25 @@ export class ProductsService {
         }
       }, 3000);
     
+    });
+
+  } 
+
+  //observable
+  public getAllProductsAsync3(): Observable<Product[]>{
+
+    return Observable.create(observer => {
+      setTimeout(() => {
+        try {
+          const arr: Product[] = [];
+          arr.push(new Product(1, "Apple", 3.5, 100));
+          arr.push(new Product(1, "Banana", 3.5, 100));
+          arr.push(new Product(1, "Peach", 3.5, 100));
+          observer.next(arr);//זה נותן לנו את הדיווח הבא
+        } catch (error) {
+          observer.error(error);//זה מדווח לנו את השגיאה
+        }
+      }, 3000);
     });
 
   } 
