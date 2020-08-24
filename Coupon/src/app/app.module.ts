0c2d8ec2b5197f2089669081c3de5eef66336d4b
matan27500n <matan27500n@gmail.com>
2020-08-24 20:24:06 +0300

    ..

--- Coupon/src/app/app.module.ts
+++ Coupon/src/app/app.module.ts
@@ -2,27 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './components/header/header.component';
-import { LayoutComponent } from './components/layout/layout.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { CompanyComponent } from './components/company/company.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    LayoutComponent,
-    FooterComponent
+    AppComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [LayoutComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
