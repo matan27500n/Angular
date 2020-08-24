c0146ba2a6d592d2c103f8d3d1e9a0f6f044634e
matan27500n <matan27500n@gmail.com>
2020-08-24 11:46:53 +0300

    ...

--- CouponSystemDemo/src/app/components/header/header.component.ts
+++ CouponSystemDemo/src/app/components/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HeaderComponent {
+  public constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'couponIcon',
+      sanitizer.bypassSecurityTrustResourceUrl('/assets/images/couponIcon.png')
+    );
   }
-
 }
