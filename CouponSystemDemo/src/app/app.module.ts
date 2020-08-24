2c58918a6625a65952b38ce76e1e649f9465e501
matan27500n <matan27500n@gmail.com>
2020-08-24 16:22:29 +0300

    design site h1

--- CouponSystemDemo/src/app/app.module.ts
+++ CouponSystemDemo/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HeaderComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
