47b1062eeaa407d1135db074f8c62a4ad5f944e7
matan27500n <matan27500n@gmail.com>
2020-08-25 10:22:47 +0300

    .

--- Coupon/src/app/services/companies.service.ts
+++ Coupon/src/app/services/companies.service.ts
@@ -1,9 +1,16 @@
+import { Company } from './../models/company';
 import { Injectable } from '@angular/core';
+import { Observable, observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompaniesService {
 
-  constructor() { }
+  public constructor(private httpClient: HttpClient) { }
+
+  public getAllCompanies(): Observable<Product[]>{
+    return this.httpClient.get<>();
+  }
 }
