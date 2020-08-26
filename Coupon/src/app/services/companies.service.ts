3947d3ba5417e63a3793551113d983e7cb9cefe4
matan27500n <matan27500n@gmail.com>
2020-08-26 11:04:56 +0300

    ץ

--- Coupon/src/app/services/companies.service.ts
+++ Coupon/src/app/services/companies.service.ts
@@ -4,13 +4,14 @@ import { Observable, observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CompaniesService {
+  public constructor(private httpClient: HttpClient) {}
 
-  public constructor(private httpClient: HttpClient) { }
-
-  public getAllCompanies(): Observable<Product[]>{
-    return this.httpClient.get<>();
+  public getAllCompanies(): Observable<Company[]> {
+    return this.httpClient.get<Company[]>('../../../assets/json/company.json', {
+      withCredentials: true,
+    });
   }
 }
