84b376178435ff584997bf2fe7d4281432b19ee4
matan27500n <matan27500n@gmail.com>
2020-09-07 17:05:09 +0300

    some of angualr and spring together

--- CouponDemo2/src/app/services/login.service.ts
+++ CouponDemo2/src/app/services/login.service.ts
@@ -1,3 +1,4 @@
+import { UrlService } from './url.service';
 import { LoginResponse } from './../../models/LoginResponse';
 import { Observable } from 'rxjs';
 import { Credentials } from './../../models/Credentials';
@@ -14,13 +15,16 @@ export class LoginService {
   public password: string;
   public isLoggedIn = false;
 
-  public constructor(private httpClient: HttpClient) {}
+  public constructor(
+    private httpClient: HttpClient,
+    private urlService: UrlService
+  ) {}
 
   public loginRequest(credentials: Credentials): Observable<LoginResponse> {
     this.email = credentials.email;
     this.password = credentials.password;
     this.type = credentials.type;
-    if (this.type === 'Admin') {
+    /*if (this.type === 'Admin') {
       return this.httpClient.get('../../assets/json/login-result.json');
     }
     if (this.type === 'Company') {
@@ -30,6 +34,13 @@ export class LoginService {
       return this.httpClient.get(
         '../../assets/json/login-result-customer.json'
       );
-    }
+    }*/
+    const correctUrl =
+      this.urlService.getUrl(credentials.type.toLowerCase()) +
+      '/login?email=' +
+      credentials.email +
+      '&password=' +
+      credentials.password;
+    return this.httpClient.post(correctUrl, null);
   }
 }
