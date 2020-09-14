08e71fe1859ab35bdf1df62af72deeca1a940503
matan27500n <matan27500n@gmail.com>
2020-09-14 13:10:08 +0300

    .

--- CouponDemo2/src/app/components/company/company.component.ts
+++ CouponDemo2/src/app/components/company/company.component.ts
@@ -1,16 +1,35 @@
 import { Credentials } from './../../../models/Credentials';
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/services/admin.service';
+import { Company } from 'src/app/models/company';
 
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
-  styleUrls: ['./company.component.css']
+  styleUrls: ['./company.component.css'],
 })
 export class CompanyComponent implements OnInit {
-
-  constructor() { }
+  public companies: Company[];
+  public constructor(private adminService: AdminService) {}
 
   ngOnInit(): void {
+    this.adminService.getCompanies().subscribe(
+      (companies) => {
+        this.companies = companies;
+      },
+      (err) => {
+        alert(err.message);
+      }
+    );
   }
-
+  public deleteCompany(id: number): void {
+    this.adminService.deleteCompany(id).subscribe(
+      (res) => {
+        this.companies = this.companies.filter((company) => company.id !== id);
+      },
+      (err) => {
+        alert(err.message);
+      }
+    );
+  }
 }
--- CouponDemo2/src/app/components/customer/customer.component.ts
+++ CouponDemo2/src/app/components/customer/customer.component.ts
@@ -1,15 +1,36 @@
+import { AdminService } from './../../services/admin.service';
+import { Customer } from './../../models/customer';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
 })
 export class CustomerComponent implements OnInit {
-
-  constructor() { }
+  public customers: Customer[];
+  constructor(private adminService: AdminService) {}
 
   ngOnInit(): void {
+    this.adminService.getCustomers().subscribe(
+      (customers) => {
+        this.customers = customers;
+      },
+      (err) => {
+        err.message;
+      }
+    );
   }
-
+  public deleteCustomer(id: number): void {
+    this.adminService.deleteCustomer(id).subscribe(
+      (res) => {
+        this.customers = this.customers.filter(
+          (customer) => customer.id !== id
+        );
+      },
+      (err) => {
+        err.message;
+      }
+    );
+  }
 }
